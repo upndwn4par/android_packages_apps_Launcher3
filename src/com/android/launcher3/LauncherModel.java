a202fd3f0ad76361fc4942fa15b149f07496e100
Clark Scheff <clark@cyngn.com>
2014-06-10 15:01:34 -0700

    Handle theme changes
    Change-Id: Ib933df8ef825ac35dfcf3207be5f803c2da18f56
    
    Conflicts:
    	src/com/android/launcher3/Launcher.java

--- src/com/android/launcher3/LauncherModel.java
+++ src/com/android/launcher3/LauncherModel.java
@@ -1229,7 +1229,7 @@ public class LauncherModel extends BroadcastReceiver {
         }
     }
 
-    private void forceReload() {
+    void forceReload() {
         resetLoadedState(true, true);
 
         // Do this here because if the launcher activity is running it will be restarted.
